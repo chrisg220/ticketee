cb21ecf5bac2e6899facd52a1f63d9e959b00681
Christopher Gonzales <chrisg220@hotmail.com>
2013-03-11 16:26:44 -0700

    Implement creating tickets for a project

--- spec/integration/editing_projects_spec.rb
+++ spec/integration/editing_projects_spec.rb
@@ -5,7 +5,6 @@ feature "Editing Projects" do
   before do
     Factory(:project, :name => "TextMate 2")
     visit "/"
-    binding.pry
     click_link "TextMate 2"
     click_link "Edit Project"
   end
