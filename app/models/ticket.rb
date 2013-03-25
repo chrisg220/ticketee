acc1c0e5e1bdd1334866f78d82371c17e52623dd
Christopher Gonzales <chrisg220@hotmail.com>
2013-03-24 21:33:20 -0700

    Users can now upload 3 files at a time

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,9 +1,11 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
-  attr_accessible :title, :description, :asset
 
-  has_attached_file :asset
+  attr_accessible :title, :description, :assets_attributes
+
+  has_many :assets
+  accepts_nested_attributes_for :assets
 
   validates :title, :presence => true
   validates :description, :presence => true,
